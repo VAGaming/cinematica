21b3048b84f9ddda8e9528897cdf1cbabc8f32b6
VAGaming <vagaming122333@gmail.com>
2026-01-19 16:59:41 +0700

    please work

--- js/app.js
+++ js/app.js
@@ -10,16 +10,27 @@ import {
   updateDoc,
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-// Your Firebase config
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyA3owRur9MWgjKY70-NlyZfOdd1IJvex4g",
+  authDomain: "cinematica-8e23f.firebaseapp.com",
+  projectId: "cinematica-8e23f",
+  storageBucket: "cinematica-8e23f.firebasestorage.app",
+  messagingSenderId: "324150998994",
+  appId: "1:324150998994:web:68288859e11a0d4ef5ba0b",
+  measurementId: "G-Y3MG5ZXYLG",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
 // Reference to collection
 const usersRef = collection(db, "users");
